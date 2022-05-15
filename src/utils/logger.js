7e2b3a26434c4cbbf4d50017b45cff8b4cfad352
Dmitry S <dmitry.s@hsdn.org>
2022-05-15 09:47:05 +0400

    Add log to file

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,19 +1,39 @@
 "use strict";
 
+const fs = require("fs");
+const path = require("path");
 const winston = require("winston");
+const moment = require("moment-timezone");
 
-module.exports = winston.createLogger({
-	level: process.env.API_LOG_LEVEL || "info",
+const logDirectory = "../../logs";
+const logger = winston.createLogger({ level: process.env.API_LOG_LEVEL || "info" });
+
+logger.add(new winston.transports.Console({
 	format: winston.format.combine(
-		winston.format.colorize(),
-		winston.format.timestamp({
-			format: "YYYY-MM-DD HH:mm:ss.SSS"
-		}),
+		winston.format.colorize({ level: true }),
+		winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
 		winston.format.printf(info =>
 			`[${info.timestamp}] ${info.level}: ${info.message}`
 		)
-	),
-	transports: [
-		new winston.transports.Console()
-	]
-});
+	)
+}));
+
+if (/^true$/i.test(process.env.API_LOG_WRITE)) {
+	const logFilename = `log_${moment.utc().local().format("YYYY-MM-DD_HH-mm-ss")}_${process.pid}.log`;
+
+	if (!fs.existsSync(path.resolve(__dirname, logDirectory))) {
+		fs.mkdirSync(path.resolve(__dirname, logDirectory));
+	}
+
+	logger.add(new winston.transports.File({
+		filename: path.resolve(__dirname, logDirectory, logFilename),
+		format: winston.format.combine(
+			winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
+			winston.format.printf(info =>
+				`[${info.timestamp}] ${info.level.toUpperCase()}: ${info.message}`
+			)
+		)
+	}));
+}
+
+module.exports = logger;
