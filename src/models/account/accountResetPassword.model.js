7faf3b5952ec84adb54855f6218c1e9d340e1685
Dmitry S <dmitry.s@hsdn.org>
2024-09-06 18:41:50 +0400

    Add accountResetPassword model

---
+++ src/models/account/accountResetPassword.model.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+* @typedef {import("../account.model").Sequelize} Sequelize
+* @typedef {import("../account.model").DataTypes} DataTypes
+*/
+
+/**
+* @param {Sequelize} sequelize
+* @param {DataTypes} DataTypes
+*/
+module.exports = (sequelize, DataTypes) =>
+	sequelize.define("account_reset_password", {
+		token: {
+			type: DataTypes.STRING(128),
+			primaryKey: true,
+			allowNull: false
+		},
+		email: {
+			type: DataTypes.STRING(64),
+			primaryKey: true,
+			allowNull: false
+		},
+		code: {
+			type: DataTypes.STRING(6)
+		},
+		failsCount: {
+			type: DataTypes.INTEGER(11),
+			allowNull: false,
+			defaultValue: 0
+		},
+		createdAt: {
+			type: DataTypes.DATE,
+			defaultValue: DataTypes.NOW
+		}
+	})
+;
