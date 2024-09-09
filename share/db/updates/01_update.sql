be470ad03176d2d65baffa553403e5a50e348f42
Dmitry S <dmitry.s@hsdn.org>
2024-09-09 18:23:26 +0400

    Update

--- share/db/updates/01_update.sql
+++ share/db/updates/01_update.sql
@@ -1,2 +1,27 @@
 
 ALTER TABLE `account_info` CHANGE COLUMN `language` `language` VARCHAR(5) NULL DEFAULT NULL COLLATE 'utf8_general_ci' AFTER `privilege`;
+
+ALTER TABLE `account_info` DROP PRIMARY KEY, ADD PRIMARY KEY (`accountDBID`) USING BTREE, ADD UNIQUE INDEX `userName` (`userName`), ADD UNIQUE INDEX `email` (`email`) ;
+ALTER TABLE `account_info` DROP INDEX `authKey`, ADD UNIQUE INDEX `authKey` (`authKey`) USING BTREE;
+
+CREATE TABLE IF NOT EXISTS `account_reset_password` (
+  `token` VARCHAR(128) NOT NULL,
+  `email` VARCHAR(64) NOT NULL,
+  `code` VARCHAR(6) NULL,
+  `failsCount` INT(11) NOT NULL DEFAULT '0',
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`token`, `email`),
+  INDEX `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `account_verify` (
+  `token` VARCHAR(128) NOT NULL,
+  `email` VARCHAR(64) NOT NULL,
+  `code` VARCHAR(6) NULL,
+  `failsCount` INT(11) NOT NULL DEFAULT '0',
+  `userName` VARCHAR(64) NULL,
+  `passWord` VARCHAR(128) NULL,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`token`, `email`),
+  INDEX `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
