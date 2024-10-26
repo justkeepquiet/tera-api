8a050b8f9859aa00bf9c6269928f4d9f0e744ec0
Dmitry S <dmitry.s@hsdn.org>
2024-10-26 20:43:06 +0400

    Fix migrations

--- src/migrations/update_000003.js
+++ src/migrations/update_000003.js
@@ -13,114 +13,99 @@ module.exports = {
 	 * @param {Sequelize} Sequelize
 	 */
 	up: async (queryInterface, Sequelize) => {
-		const transaction = await queryInterface.sequelize.transaction();
+		// `account_bans`
+		await queryInterface.removeConstraint("account_bans", "PRIMARY");
+		await queryInterface.addColumn("account_bans", "id", {
+			type: Sequelize.DataTypes.BIGINT(20),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			first: true
+		});
+		await queryInterface.addIndex("account_bans", ["accountDBID"], {
+			unique: false,
+			name: "accountDBID"
+		});
 
-		try {
-			// `account_bans`
-			await queryInterface.removeConstraint("account_bans", "PRIMARY", { transaction });
-			await queryInterface.addColumn("account_bans", "id", {
-				type: Sequelize.DataTypes.BIGINT(20),
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				first: true
-			}, { transaction });
-			await queryInterface.addIndex("account_bans", ["accountDBID"], {
-				unique: false,
-				name: "accountDBID",
-				transaction
-			});
+		// `account_info`
+		await queryInterface.changeColumn("account_info", "language", {
+			type: Sequelize.DataTypes.STRING(5)
+		});
+		await queryInterface.sequelize.query("ALTER TABLE `account_info` DROP PRIMARY KEY, ADD PRIMARY KEY (`accountDBID`)");
+		await queryInterface.addIndex("account_info", ["userName"], {
+			unique: true,
+			name: "userName"
+		});
+		await queryInterface.addIndex("account_info", ["email"], {
+			unique: true,
+			name: "email"
+		});
+		await queryInterface.removeIndex("account_info", "authKey");
+		await queryInterface.addIndex("account_info", ["authKey"], {
+			unique: true,
+			name: "authKey"
+		});
 
-			// `account_info`
-			await queryInterface.changeColumn("account_info", "language", {
-				type: Sequelize.DataTypes.STRING(5)
-			}, { transaction });
-			await queryInterface.sequelize.query("ALTER TABLE `account_info` DROP PRIMARY KEY, ADD PRIMARY KEY (`accountDBID`)", { transaction });
-			await queryInterface.addIndex("account_info", ["userName"], {
-				unique: true,
-				name: "userName",
-				transaction
-			});
-			await queryInterface.addIndex("account_info", ["email"], {
-				unique: true,
-				name: "email",
-				transaction
-			});
-			await queryInterface.removeIndex("account_info", "authKey", { transaction });
-			await queryInterface.addIndex("account_info", ["authKey"], {
-				unique: true,
-				name: "authKey",
-				transaction
-			});
+		// `shop_promocode_strings`
+		await queryInterface.sequelize.query("ALTER TABLE `shop_promocode_strings` DROP PRIMARY KEY, DROP INDEX `id`, ADD PRIMARY KEY (`id`)");
+		await queryInterface.addIndex("shop_promocode_strings", ["language", "promoCodeId"], {
+			unique: true,
+			name: "unique"
+		});
 
-			// `shop_promocode_strings`
-			await queryInterface.sequelize.query("ALTER TABLE `shop_promocode_strings` DROP PRIMARY KEY, DROP INDEX `id`, ADD PRIMARY KEY (`id`)", { transaction });
-			await queryInterface.addIndex("shop_promocode_strings", ["language", "promoCodeId"], {
-				unique: true,
-				name: "unique",
-				transaction
-			});
+		// `shop_category_strings`
+		await queryInterface.sequelize.query("ALTER TABLE `shop_category_strings` DROP PRIMARY KEY, DROP INDEX `id`, ADD PRIMARY KEY (`id`)");
+		await queryInterface.addIndex("shop_category_strings", ["language", "categoryId"], {
+			unique: true,
+			name: "unique"
+		});
 
-			// `shop_category_strings`
-			await queryInterface.sequelize.query("ALTER TABLE `shop_category_strings` DROP PRIMARY KEY, DROP INDEX `id`, ADD PRIMARY KEY (`id`)", { transaction });
-			await queryInterface.addIndex("shop_category_strings", ["language", "categoryId"], {
-				unique: true,
-				name: "unique",
-				transaction
-			});
+		// `server_info`
+		await queryInterface.renameColumn("server_info", "tresholdLow", "thresholdLow");
+		await queryInterface.renameColumn("server_info", "tresholdMedium", "thresholdMedium");
 
-			// `server_info`
-			await queryInterface.renameColumn("server_info", "tresholdLow", "thresholdLow", { transaction });
-			await queryInterface.renameColumn("server_info", "tresholdMedium", "thresholdMedium", { transaction });
+		// `report_activity`
+		await queryInterface.addColumn("report_activity", "id", {
+			type: Sequelize.DataTypes.BIGINT(20),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			first: true
+		});
 
-			// `report_activity`
-			await queryInterface.addColumn("report_activity", "id", {
-				type: Sequelize.DataTypes.BIGINT(20),
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				first: true
-			}, { transaction });
+		// `report_characters`
+		await queryInterface.addColumn("report_characters", "id", {
+			type: Sequelize.DataTypes.BIGINT(20),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			first: true
+		});
 
-			// `report_characters`
-			await queryInterface.addColumn("report_characters", "id", {
-				type: Sequelize.DataTypes.BIGINT(20),
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				first: true
-			}, { transaction });
+		// `report_cheats`
+		await queryInterface.addColumn("report_cheats", "id", {
+			type: Sequelize.DataTypes.BIGINT(20),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			first: true
+		});
 
-			// `report_cheats`
-			await queryInterface.addColumn("report_cheats", "id", {
-				type: Sequelize.DataTypes.BIGINT(20),
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				first: true
-			}, { transaction });
+		// `report_chronoscrolls`
+		await queryInterface.addColumn("report_chronoscrolls", "id", {
+			type: Sequelize.DataTypes.BIGINT(20),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			first: true
+		});
 
-			// `report_chronoscrolls`
-			await queryInterface.addColumn("report_chronoscrolls", "id", {
-				type: Sequelize.DataTypes.BIGINT(20),
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				first: true
-			}, { transaction });
-
-			// `report_shop_pay`
-			await queryInterface.addColumn("report_shop_pay", "quantity", {
-				type: Sequelize.DataTypes.INTEGER(11),
-				allowNull: false,
-				defaultValue: 1
-			}, { transaction });
-
-			await transaction.commit();
-		} catch (err) {
-			await transaction.rollback();
-			throw err;
-		}
+		// `report_shop_pay`
+		await queryInterface.addColumn("report_shop_pay", "quantity", {
+			type: Sequelize.DataTypes.INTEGER(11),
+			allowNull: false,
+			defaultValue: 1
+		});
 	},
 
 	/**
@@ -128,73 +113,46 @@ module.exports = {
 	 * @param {Sequelize} Sequelize
 	 */
 	down: async (queryInterface, Sequelize) => {
-		const transaction = await queryInterface.sequelize.transaction();
+		// `account_bans`
+		await queryInterface.sequelize.query("ALTER TABLE `account_bans` DROP COLUMN `id`, DROP PRIMARY KEY, DROP INDEX `accountDBID`, ADD PRIMARY KEY (`accountDBID`)");
 
-		try {
-			// `account_bans`
-			await queryInterface.removeConstraint("account_bans", "PRIMARY", { transaction });
-			await queryInterface.removeColumn("account_bans", "id", { transaction });
-			await queryInterface.removeIndex("account_bans", "accountDBID", { transaction });
-			await queryInterface.addConstraint("account_bans", {
-				fields: ["accountDBID"],
-				type: "primary key",
-				transaction
-			});
+		// `account_info`
+		await queryInterface.sequelize.query("ALTER TABLE `account_info` DROP PRIMARY KEY, DROP INDEX `userName`, ADD PRIMARY KEY (`accountDBID`, `userName`)");
+		await queryInterface.removeIndex("account_info", "email");
+		await queryInterface.removeIndex("account_info", "authKey");
+		await queryInterface.addIndex("account_info", ["authKey"], {
+			unique: false,
+			name: "authKey"
+		});
+		await queryInterface.changeColumn("account_info", "language", {
+			type: Sequelize.DataTypes.STRING(3)
+		});
 
-			// `account_info`
-			await queryInterface.changeColumn("account_info", "language", {
-				type: Sequelize.DataTypes.STRING(3)
-			}, { transaction });
-			await queryInterface.removeIndex("account_info", "userName", { transaction });
-			await queryInterface.removeIndex("account_info", "email", { transaction });
-			await queryInterface.sequelize.query("ALTER TABLE `account_info` DROP PRIMARY KEY, ADD PRIMARY KEY (`accountDBID`, `userName`)", { transaction });
-			await queryInterface.addIndex("account_info", ["authKey"], {
-				unique: false,
-				name: "authKey",
-				transaction
-			});
+		// `shop_promocode_strings`
+		await queryInterface.removeIndex("shop_promocode_strings", "unique");
+		await queryInterface.sequelize.query("ALTER TABLE `shop_promocode_strings` DROP PRIMARY KEY, ADD UNIQUE INDEX `id` (`id`), ADD PRIMARY KEY (`language`, `promoCodeId`)");
 
-			// `shop_promocode_strings`
-			await queryInterface.removeIndex("shop_promocode_strings", "unique", { transaction });
-			await queryInterface.sequelize.query("ALTER TABLE `shop_promocode_strings` DROP PRIMARY KEY, DROP INDEX `id`, ADD PRIMARY KEY (`language`, `promoCodeId`)", { transaction });
-			await queryInterface.addIndex("shop_promocode_strings", ["id"], {
-				unique: true,
-				name: "id",
-				transaction
-			});
+		// `shop_category_strings`
+		await queryInterface.removeIndex("shop_category_strings", "unique");
+		await queryInterface.sequelize.query("ALTER TABLE `shop_category_strings` DROP PRIMARY KEY, ADD UNIQUE INDEX `id` (`id`), ADD PRIMARY KEY (`language`, `categoryId`)");
 
-			// `shop_category_strings`
-			await queryInterface.removeIndex("shop_category_strings", "unique", { transaction });
-			await queryInterface.sequelize.query("ALTER TABLE `shop_category_strings` DROP PRIMARY KEY, DROP INDEX `id`, ADD PRIMARY KEY (`language`, `categoryId`)", { transaction });
-			await queryInterface.addIndex("shop_category_strings", ["id"], {
-				unique: true,
-				name: "id",
-				transaction
-			});
+		// `server_info`
+		await queryInterface.renameColumn("server_info", "thresholdLow", "tresholdLow");
+		await queryInterface.renameColumn("server_info", "thresholdMedium", "tresholdMedium");
 
-			// `server_info`
-			await queryInterface.renameColumn("server_info", "thresholdLow", "tresholdLow", { transaction });
-			await queryInterface.renameColumn("server_info", "thresholdMedium", "tresholdMedium", { transaction });
+		// `report_activity`
+		await queryInterface.removeColumn("report_activity", "id");
 
-			// `report_activity`
-			await queryInterface.removeColumn("report_activity", "id", { transaction });
+		// `report_characters`
+		await queryInterface.removeColumn("report_characters", "id");
 
-			// `report_characters`
-			await queryInterface.removeColumn("report_characters", "id", { transaction });
+		// `report_cheats`
+		await queryInterface.removeColumn("report_cheats", "id");
 
-			// `report_cheats`
-			await queryInterface.removeColumn("report_cheats", "id", { transaction });
+		// `report_chronoscrolls`
+		await queryInterface.removeColumn("report_chronoscrolls", "id");
 
-			// `report_chronoscrolls`
-			await queryInterface.removeColumn("report_chronoscrolls", "id", { transaction });
-
-			// `report_shop_pay`
-			await queryInterface.removeColumn("report_shop_pay", "quantity", { transaction });
-
-			await transaction.commit();
-		} catch (err) {
-			await transaction.rollback();
-			throw err;
-		}
+		// `report_shop_pay`
+		await queryInterface.removeColumn("report_shop_pay", "quantity");
 	}
 };
