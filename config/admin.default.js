9bd05114fd228e8b76cf1b6c6e21308844277c75
Dmitry S <dmitry.s@hsdn.org>
2025-01-13 17:30:41 +0400

    Remove shop locales

--- config/admin.default.js
+++ config/admin.default.js
@@ -8,12 +8,5 @@ module.exports = {
 		{ name: "WebApp Panel", url: "http://localhost:88/" },
 		{ name: "Box System Panel", url: "http://localhost:8070/" },
 		{ name: "Steer Server Panel", url: "http://localhost:8060/" }
-	],
-
-	// List of locales (languages) supported by the Shop.
-	// This list is only used to display strings settings in the Admin Panel.
-	shopLocales: [
-		"en",
-		"ru"
 	]
 };
