08ff55220cf4e4fa77fa5ce4e8fed69ca1ffd409
Dmitry S <dmitry.s@hsdn.org>
2025-01-21 19:39:30 +0400

    Fix benefit handler

--- src/actions/handlers/benefit.js
+++ src/actions/handlers/benefit.js
@@ -17,52 +17,60 @@ class Benefit {
 		this.params = params;
 	}
 
-	addBenefit(benefitId, days) {
-		return this.modules.accountModel.benefits.findOne({
+	async addBenefit(benefitId, days) {
+		const benefit = await this.modules.accountModel.benefits.findOne({
 			attributes: ["availableUntil", [this.modules.sequelize.fn("NOW"), "dateNow"]],
 			where: { accountDBID: this.userId, benefitId }
-		}).then(benefit => {
-			let promise = null;
-
-			if (benefit === null) {
-				promise = this.modules.accountModel.benefits.create({
-					accountDBID: this.userId,
-					benefitId,
-					availableUntil: this.modules.sequelize.fn("ADDDATE",
-						this.modules.sequelize.fn("NOW"), days)
-				});
-			} else {
-				const currentDate = moment(benefit.get("dateNow")).isAfter(benefit.get("availableUntil")) ?
-					this.modules.sequelize.fn("NOW") :
-					this.modules.sequelize.col("availableUntil");
-
-				promise = this.modules.accountModel.benefits.update({
-					availableUntil: this.modules.sequelize.fn("ADDDATE", currentDate, days)
-				}, {
-					where: { accountDBID: this.userId, benefitId }
-				});
-			}
-
-			return promise.then(() => {
-				let totalDays = days;
-
-				if (benefit !== null) {
-					totalDays += Math.round(
-						moment.duration(moment(benefit.get("availableUntil"))
-							.diff(moment(benefit.get("dateNow"))))
-							.asDays()
-					);
-				}
-
-				if (this.serverId !== null) {
-					this.modules.hub.addBenefit(this.serverId, this.userId, benefitId, totalDays * 86400).catch(err =>
-						this.modules.logger.warn(err.toString())
-					);
-				}
-
-				return Promise.resolve();
-			});
 		});
+
+		let totalDays = parseInt(days);
+
+		if (benefit === null) {
+			await this.modules.accountModel.benefits.create({
+				accountDBID: this.userId,
+				benefitId,
+				availableUntil: this.modules.sequelize.fn("ADDDATE",
+					this.modules.sequelize.fn("NOW"), totalDays)
+			});
+		} else {
+			const currentDate = moment(benefit.get("dateNow")).isAfter(benefit.get("availableUntil")) ?
+				this.modules.sequelize.fn("NOW") :
+				this.modules.sequelize.col("availableUntil");
+
+			await this.modules.accountModel.benefits.update({
+				availableUntil: this.modules.sequelize.fn("ADDDATE", currentDate, totalDays)
+			}, {
+				where: { accountDBID: this.userId, benefitId }
+			});
+
+			totalDays += Math.round(moment.duration(moment(benefit.get("availableUntil"))
+				.diff(moment(benefit.get("dateNow"))))
+				.asDays());
+		}
+
+		if (this.serverId !== null) {
+			this.modules.hub.addBenefit(this.serverId, this.userId, benefitId, totalDays * 86400).catch(err =>
+				this.modules.logger.warn(err.toString())
+			);
+		}
+	}
+
+	async removeBenefit(benefitId) {
+		const benefit = await this.modules.accountModel.benefits.findOne({
+			where: { accountDBID: this.userId, benefitId }
+		});
+
+		if (benefit !== null) {
+			await this.modules.accountModel.benefits.destroy({
+				where: { benefitId, accountDBID: this.userId }
+			});
+
+			if (this.serverId !== null) {
+				this.modules.hub.removeBenefit(this.serverId, this.userId, benefitId).catch(err =>
+					this.modules.logger.warn(err.toString())
+				);
+			}
+		}
 	}
 }
 
