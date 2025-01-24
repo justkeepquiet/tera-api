f5f7d22891dc1ce07600dba2a7d0d26b64a09439
Dmitry S <dmitry.s@hsdn.org>
2025-01-24 07:09:14 +0400

    Add generateRandomWord

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -223,6 +223,20 @@ module.exports.generateVerificationCode = () =>
 ;
 
 /**
+ * @param {string} length
+ * @return {string}
+ */
+module.exports.generateRandomWord = length => {
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	let result = "";
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		result += characters[randomIndex];
+	}
+	return result;
+};
+
+/**
 * @param {string} email
 * @return {string}
 */
