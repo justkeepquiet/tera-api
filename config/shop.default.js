2651845d3cd0f4c7c311b1aaaa279043a80b17b1
Dmitry S <dmitry.s@hsdn.org>
2025-01-26 04:56:31 +0400

    Remove unused parameter

--- config/shop.default.js
+++ config/shop.default.js
@@ -3,16 +3,6 @@
 // THE CHANGES MADE ARE APPLIED WITHOUT RESTARTING THE PROCESS.
 
 module.exports = {
-	// Customizing the slides carousel on the shop welcome page
-	// Slides can be uploaded to "public\shop\images" directory and must be resolution 740x300 px.
-	// A product ID can be provided for the link reference.
-	welcomeCarousel: [
-		{ slide: "/public/shop/images/slide-01.jpg", productId: "195" },
-		{ slide: "/public/shop/images/slide-02.jpg", productId: "185" },
-		{ slide: "/public/shop/images/slide-03.jpg", productId: "97" },
-		{ slide: "/public/shop/images/slide-04.jpg", productId: "162" }
-	],
-
 	// List of Founder Benefit IDs to display on the shop welcome page
 	founderBenefits: [
 		"334",
